5e06214e97143adccd064865bfb9ef1963fad1fe
Grozdanovsky <viktor.grozdanovski@outlook.com>
2021-02-20 18:08:16 +0100

    first commit

---
+++ igrac.py
@@ -0,0 +1,9 @@
+
+
+class Igrac:
+    def __init__(self,ime,prezime,broj,pozicija,ppg):
+        self.__ime = ime
+        self.__prezime = prezime
+        self.__broj = broj
+        self.__pozicija = pozicija
+        self.__ppg = ppg
